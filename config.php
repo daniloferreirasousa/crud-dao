f2221a8193024914c06d39baf009835c54c8be6d
Danilo Ferreira <106565796+daniloferreirasousa@users.noreply.github.com>
2023-04-11 08:14:40 -0300

    Atualização do método de conexão com banco de dados, inserido 'try:catch'

--- config.php
+++ config.php
@@ -1,14 +1,16 @@
 <?php
-    /*
-        C -> CREATE
-        R -> READ
-        U -> UPDATE
-        D -> DELETE
-    */
+
 $db_name = 'crm';
-$db_host = 'localhost';
+$db_host = '127.0.0.1';
+$db_port = '1010';
 $db_user = 'root';
 $db_pass = ''; 
 
-
-$pdo = new PDO("mysql:dbname={$db_name};host={$db_host};", $db_user, $db_pass);
+try {
+    $pdo = new PDO("mysql:dbname={$db_name};host={$db_host};port={$db_port};", $db_user, $db_pass);
+} catch (Exception $e) {
+    echo "<pre>";
+    print_r($e);
+    echo "</pre>";
+    die();
+}
